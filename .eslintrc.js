fcef172bb4a3da9ca3f362a06433bdb8833cdb8c
charshin <chaunhihien@gmail.com>
2019-08-23 11:29:52 +0800

    Initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: 'babel-eslint',
+  plugins: ['prettier', 'react-hooks', 'jest', 'import', 'babel'],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  env: {
+    'jest/globals': true,
+    browser: true,
+  },
+  // use eslint-import-resolver-webpack to parse alias in webpack
+  settings: {
+    'import/resolver': 'webpack',
+  },
+  rules: {
+    'prettier/prettier': ['error'],
+    'react/react-in-jsx-scope': 'off', // Handled by babel-plugin-react-require
+    'react/jsx-filename-extension': 'off',
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    // doesn't fail when using do expressions or optional chaining
+    'no-unused-expressions': 'off',
+    'babel/no-unused-expressions': 'error',
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  output: {
+    library: 'ReduxFormSemanticUI',
+    libraryTarget: 'umd',
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
+    redux: {
+      root: 'Redux',
+      commonjs2: 'redux',
+      commonjs: 'redux',
+      amd: 'redux',
+    },
+    'react-redux': {
+      root: 'ReactRedux',
+      commonjs2: 'react-redux',
+      commonjs: 'react-redux',
+      amd: 'react-redux',
+    },
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'production',
+    }),
+  ],
+};

4b3b4bf57bad2fd7de4660a7a86289e0c2632af9
Zeke Tan <zeke.tan@snaphunt.com>
2020-03-31 12:52:27 +0800

    🧹Update textarea to new structure

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,42 +1,73 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import {
   Form as SuiForm,
   TextArea as SuiTextArea,
   Popup as SuiPopup,
 } from 'semantic-ui-react';
-import { Field } from 'redux-form';
+import { Field, fieldPropTypes } from 'redux-form';
 
-const TextArea = ({ name, ...props }) => {
-  const render = useCallback(
-    ({ readonly, size, ...fieldProps }) => {
-      const renderView = () => {
-        const {
-          input: { value },
-          label,
-          hidden,
-          colspan,
-          textareaProps,
-        } = fieldProps;
-
-        return (
-          <SuiForm.Field
-            width={colspan}
-            style={{ visibility: hidden ? 'hidden' : 'visible' }}
-          >
-            {label && (
-              <label htmlFor={name} style={{ whiteSpace: 'pre' }}>
-                {label}
-              </label>
-            )}
+const TextArea = ({
+  name,
+  readonly,
+  input,
+  meta: { touched, error, active },
+  label,
+  required,
+  disabled,
+  hidden,
+  colspan,
+  textareaProps,
+}) => {
+  return readonly ? (
+    <SuiForm.Field
+      width={colspan}
+      style={{ visibility: hidden ? 'hidden' : 'visible' }}
+    >
+      {label && (
+        <label htmlFor={name} style={{ whiteSpace: 'pre' }}>
+          {label}
+        </label>
+      )}
+      <SuiTextArea
+        {...textareaProps}
+        id={name}
+        // * Default to empty string to ensure always in controlled mode
+        value={input?.value || ''}
+        readOnly={readonly}
+        css={{
+          '.ui.form textarea&': {
+            ...(textareaProps?.resize && {
+              resize: textareaProps.resize,
+            }),
+          },
+        }}
+      />
+    </SuiForm.Field>
+  ) : (
+    <SuiForm.Field
+      error={touched && !!error}
+      required={required}
+      disabled={disabled}
+      width={colspan}
+      style={{ visibility: hidden ? 'hidden' : 'visible' }}
+    >
+      {label && (
+        <label htmlFor={name} style={{ whiteSpace: 'pre' }}>
+          {label}
+        </label>
+      )}
+      <SuiPopup
+        trigger={
+          // ? This wrapper is necessary for 'poppper' to work with '@emotion/core'
+          <div>
             <SuiTextArea
               {...textareaProps}
+              {...input}
               id={name}
               // * Default to empty string to ensure always in controlled mode
-              value={value || ''}
-              readOnly={readonly}
+              value={input?.value || ''}
               css={{
                 '.ui.form textarea&': {
                   ...(textareaProps?.resize && {
@@ -45,69 +76,14 @@ const TextArea = ({ name, ...props }) => {
                 },
               }}
             />
-          </SuiForm.Field>
-        );
-      };
-
-      const renderEdit = () => {
-        const {
-          input,
-          meta: { touched, error, active },
-          label,
-          required,
-          disabled,
-          hidden,
-          colspan,
-          textareaProps,
-        } = fieldProps;
-
-        return (
-          <SuiForm.Field
-            error={touched && !!error}
-            required={required}
-            disabled={disabled}
-            width={colspan}
-            style={{ visibility: hidden ? 'hidden' : 'visible' }}
-          >
-            {label && (
-              <label htmlFor={name} style={{ whiteSpace: 'pre' }}>
-                {label}
-              </label>
-            )}
-            <SuiPopup
-              trigger={
-                // ? This wrapper is necessary for 'poppper' to work with '@emotion/core'
-                <div>
-                  <SuiTextArea
-                    {...textareaProps}
-                    {...input}
-                    id={name}
-                    // * Default to empty string to ensure always in controlled mode
-                    value={input?.value || ''}
-                    css={{
-                      '.ui.form textarea&': {
-                        ...(textareaProps?.resize && {
-                          resize: textareaProps.resize,
-                        }),
-                      },
-                    }}
-                  />
-                </div>
-              }
-              content={error}
-              style={{ opacity: !active && touched && !!error ? 0.7 : 0 }}
-              inverted
-            />
-          </SuiForm.Field>
-        );
-      };
-
-      return readonly ? renderView() : renderEdit();
-    },
-    [name],
+          </div>
+        }
+        content={error}
+        style={{ opacity: !active && touched && !!error ? 0.7 : 0 }}
+        inverted
+      />
+    </SuiForm.Field>
   );
-
-  return <Field {...props} name={name} component={render} />;
 };
 
 TextArea.defaultProps = {
@@ -118,6 +94,7 @@ TextArea.defaultProps = {
 };
 
 TextArea.propTypes = {
+  ...fieldPropTypes,
   name: PropTypes.string.isRequired,
   label: PropTypes.string,
   disabled: PropTypes.bool,
@@ -129,4 +106,4 @@ TextArea.propTypes = {
   }),
 };
 
-export default TextArea;
+export default props => <Field {...props} component={TextArea} />;
